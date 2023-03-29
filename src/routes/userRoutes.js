095c844ed57f065c74a74c613aa19c140001039e
Aderibigbe Jesutoni Jesutowo <aderibigbejesutoni860@gmail.com>
2023-03-29 10:21:12 +0100

    first commit

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -12,16 +12,16 @@ router.get('/', adminKey, userController.getUsers) //Get Users
 router.delete('/:id', authMiddleware, userController.deleteUsers); //Delete Users
 
 router.put("/api/v1/:id", authMiddleware, userController.updateUsers); //Update Users
-router.post("api/v1/:userId/orders", authMiddleware, limiter, userController.addProductforUser); //Add Orders to users cart
-router.delete("api/v1/:userId/orders/:orderId", authMiddleware,userController.deleteOrderForUser); //Delete Orders from users cart
-router.get("api/v1/:email", authMiddleware, userController.getUsersbyId);  // Get Users by EmailId to know what is in the orders cart
-router.put("api/v1/:userId", userController.updateOrderforUser); //Update Orders from users cart
-router.get("api/v1/orders/:email", userController.getOrdersforUser); //Get Orders for specific users
-router.post("api/v1/password", userController.resetPassword); //Forgot Password for Users
+router.post("/api/v1/:userId/orders", authMiddleware, limiter, userController.addProductforUser); //Add Orders to users cart
+router.delete("/api/v1/:userId/orders/:orderId", authMiddleware,userController.deleteOrderForUser); //Delete Orders from users cart
+router.get("/api/v1/:email", authMiddleware, userController.getUsersbyId);  // Get Users by EmailId to know what is in the orders cart
+router.put("/api/v1/:userId", userController.updateOrderforUser); //Update Orders from users cart
+router.get("/api/v1/orders/:email", userController.getOrdersforUser); //Get Orders for specific users
+router.post("/api/v1/password", userController.resetPassword); //Forgot Password for Users
 
-router.post("api/v1/orders/:email/pay", limiter, authMiddleware, userController.makePayment);//User making payments for the orders
+router.post("/api/v1/orders/:email/pay", limiter, authMiddleware, userController.makePayment);//User making payments for the orders
 
-router.get("api/v1/:userId/orders/price", userController.getOrdersAndPrice);
+router.get("/api/v1/:userId/orders/price", userController.getOrdersAndPrice);
 //Get all Orders for admin
 
 
